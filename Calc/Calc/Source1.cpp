6f5d9dbd5c5ffa9e4e99830bd2e73fae644330bf
primrous02 <vadym.prymary@gmail.com>
2021-09-05 21:48:22 +0300

    Completing the program...

--- Calc/Calc/Source1.cpp
+++ Calc/Calc/Source1.cpp
@@ -5,6 +5,13 @@ int main2() {
 	double a, b, c;
 	char d;
 	int p = 1;
+	while (p != 2) {
+		printf("1 - Calculator\n2 - Exit\n");
+		scanf("%d", &p);
+		if (p != 1) break;
+		printf("Write expression (Without spaces!): ");
+		scanf("%lf%c%%lf", &a, &d, &b);
+	}
 
 
 }
