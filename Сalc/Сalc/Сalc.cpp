aca4f47568e59c0abcf1c1efd09e73401e258fb2
primrous02 <vadym.prymary@gmail.com>
2021-09-05 22:23:02 +0300

    Recreated project and added _CRT_SECURE_NO_WARNING for avoiding problems while launch

---
+++ Сalc/Сalc/Сalc.cpp
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	double a, b, c;
+	char d;
+	int p = 1;
+	while (p != 2) {
+		printf("1 - Calculator\n2 - Exit\n");
+		scanf("%d", &p);
+		if (p != 1) break;
+		printf("Write expression (Without spaces!): ");
+		scanf("%lf%c%lf", &a, &d, &b);
+		while (getchar() != '\n');
+		switch (d) {
+		case '+': c = a + b; break;
+		case '-': c = a - b; break;
+		case '*': c = a * b; break;
+		case '/': c = a / b; break;
+		default:
+			printf("Wrong action\n");
+		}
+		printf("Result: %lf\n", c);
+	}
+}
